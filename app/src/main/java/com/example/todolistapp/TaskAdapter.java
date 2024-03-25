885ca2ea0578b2c182c320c9a39c23d7720113de
Emmanuel Oduor <eooduor31@gmail.com>
2024-03-25 19:19:09 +0300

    Set up the UI for the login Activity

--- app/src/main/java/com/example/todolistapp/TaskAdapter.java
+++ app/src/main/java/com/example/todolistapp/TaskAdapter.java
@@ -12,12 +12,14 @@ import java.util.List;
 
 public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskViewHolder> {
 
-    private List<Task> taskList;
+    private List<Task> taskList; // Data source for the adapter
 
+    // Constructor to initialize the adapter with a list of tasks
     public TaskAdapter(List<Task> taskList) {
-        this.taskList = taskList;
+        this.taskList = taskList;// Set the provided list of tasks to the adapter's data source
     }
 
+    // This method inflates the layout for an individual task item
     @NonNull
     @Override
     public TaskViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -26,6 +28,7 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskViewHolder
         return new TaskViewHolder(view);
     }
 
+    // This method binds data to the views within the view holder
     @Override
     public void onBindViewHolder(@NonNull TaskViewHolder holder, int position) {
         Task task = taskList.get(position);
@@ -33,14 +36,17 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskViewHolder
 
     }
 
+    // Returns the number of tasks in the data source
     @Override
     public int getItemCount() {
         return taskList.size();
     }
 
+    // Inner class representing the view holder for each task item
     public class TaskViewHolder extends RecyclerView.ViewHolder {
-        public TextView taskTitleTextView;
+        public TextView taskTitleTextView;// TextView to display task title
 
+        // Constructor to initialize the view holder
         public TaskViewHolder(@NonNull View itemView) {
             super(itemView);
             taskTitleTextView = itemView.findViewById(R.id.taskTitleTextView);
